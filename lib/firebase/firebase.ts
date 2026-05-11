e3eb6fdd43c277a8207be62292822c33b4521d60
ammar shtayeh <ammar.shtayeh@gmail.com>
2026-05-11 23:16:58 +0300

    Add Firebase client config fallback

--- lib/firebase/firebase.ts
+++ lib/firebase/firebase.ts
@@ -4,14 +4,28 @@ import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 import { getStorage } from "firebase/storage";
 
+const defaultFirebaseConfig = {
+  apiKey: "AIzaSyDueGK2bjZr6PheMUudDLxWivjNf2sD_cc",
+  authDomain: "cosmetics-association.firebaseapp.com",
+  projectId: "cosmetics-association",
+  storageBucket: "cosmetics-association.firebasestorage.app",
+  messagingSenderId: "949459961103",
+  appId: "1:949459961103:web:3445bdfdabcb050d1e6a7a",
+  measurementId: "G-XSQSWNWW4L"
+};
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || defaultFirebaseConfig.apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || defaultFirebaseConfig.authDomain,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || defaultFirebaseConfig.projectId,
+  storageBucket:
+    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || defaultFirebaseConfig.storageBucket,
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ||
+    defaultFirebaseConfig.messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || defaultFirebaseConfig.appId,
+  measurementId:
+    process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || defaultFirebaseConfig.measurementId
 };
 
 export const isFirebaseClientConfigured = Boolean(
